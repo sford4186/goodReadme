b53d3d05a577e3d9f36467cd3b42b6dbd7d3321a
Shawanda Ford <shawanda.ford@gmail.com>
2020-09-17 21:46:21 -0400

    updated prompts

--- index.js
+++ index.js
@@ -1,15 +1,99 @@
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs")
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,generateMarkdown(data),err=>{
+        if(err) throw err;
+        console.log('readme created')
+    })
+}
+
+// function to initialize program
+function init() {
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+            {
+                type: "input",
+                message: "What is the title of your project?",
+                name: "title",
+            },
+            {
+                type: "input",
+                message: "Create a description",
+                name: "description",
+            },
+
+
+
+            //questionable
+            {
+                type: "input",
+                message: "Usage",
+                name: "usage",
+            },
+
+            //questionable
+            {
+                type: "list",
+                message: "What is the name of the license you used?",
+                name: "license",
+                choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+            },
+            //questionable
+            {
+                type: "input",
+                message: "Contributions?",
+                name: "contributing",
+            },
+            //questionable
+            {
+                type: "input",
+                message: "Did you run a test?",
+                name: "test",
+                default: "npm test"
+            },
+            {
+                type: "input",
+                message: "How do you install this application?",
+                name: "install",
+                default: "npm i"
+            },
+            //questionable
+            {
+                type: "input",
+                message: "Enter your GitHub Username",
+                name: "question1",
+            },
+            {
+                type: "input",
+                message: "Enter your email address",
+                name: "email",
+            },
+            //questionable
+
+        ])
+        .then(answers => {
+            // Use user feedback for... whatever!!
+            console.log("Success!")
+            answers.license = encodeURI(answers.license)
+            writeToFile(`${answers.title}.md`, answers)
+        })
+        // .catch(error => {
+        //     if (error.isTtyError) {
+        //         // Prompt couldn't be rendered in the current environment
+        //         console.log("Error!")
+        //     } else {
+        //         // Something else when wrong
+        //         console.log("Re-check your message")
+        //     }
+        // });
+
+
+}
+
+// function call to initialize program
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,39 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}  
+![${data.license}]($(https://img.shields.io/badge/License-${data.license}-purple)  
+## Description:  
+${data.description}
+## Table of Contents 
+  * [Installation](#installation)
+  * [Usage](#usage)  
+  * [License](#license)  
+  * [Contributing](#contributing)  
+  * [Test](#test)  
+  * [Questions](#questions)  
+
+## Installation  
+To install this, you will need to run:  
+${data.install}  
+  
+##  Usage  
+To use this application: 
+${data.usage}  
+  
+##  License  
+This project is licensed under:  
+${data.license}  
+  
+##  Contributing  
+Is this project accepting contributions:  
+${data.contributing}  
+  
+## Questions  
+If you hav questions about the repo email me at ${data.email}. You can see more of my work on [github.](https://www.github.com/${data.question1})
+
+
+
+`;
+}
+
+module.exports = generateMarkdown;
